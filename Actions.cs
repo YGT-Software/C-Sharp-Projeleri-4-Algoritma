68ab49b033d8bffff43f0351468ca2c0772affcc
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-02-28 13:52:48 +0300

    Algoritma Projesi Tamamlandı

---
+++ Actions.cs
@@ -0,0 +1,13 @@
+namespace C__Projeleri_4_Algoritma;
+public class Actions
+{
+    public void Algoritma(string ifade)
+    {
+        string[] input = ifade.Split(",");
+        string word = input[0];
+        int number = Convert.ToInt32(input[1]);
+        string newWord = word.Remove(number, 1);
+
+        Console.WriteLine(newWord);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace C__Projeleri_4_Algoritma;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir kelime ve bir sayı giriniz (Arada virgül olmalıdır):");
+        string ifade = Console.ReadLine();
+
+        Actions actions = new Actions();
+        actions.Algoritma(ifade);
+    }
+}
